c1618c3bee9796f370558d5636fe838a258ded86
hellosaraann <srichardson@mcad.edu>
2019-02-14 20:23:28 -0600

    Added assignment javascript draft 1
    Added required array, objects and functions tested on Codepen

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 document.getElementById('BtnDonate').addEventListener('click', function(){
+  console.log("There are " + greatSpeeches.length + " speeches on the page.");
   //Code in here executes when the user clicks the "Donate" button.
 });
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
+  console.log("This speech was written by " + greatSpeeches[0].author + " in " + greatSpeeches[0].year + ". It is " + greatSpeeches[0].bce + " that this year is B.C.E.");
   //Code in here executes when the user clicks the "Churchill" button.
 });
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
+  console.log("This speech was written by " + greatSpeeches[1].author + " in " + greatSpeeches[1].year + ". It is " + greatSpeeches[1].bce + " that this year is B.C.E.");
   //Code in here executes when the user clicks the "Ghandi" button.
 });
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
+  console.log("This speech was written by " + greatSpeeches[2].author + " in " + greatSpeeches[2].year + ". It is " + greatSpeeches[2].bce + " that this year is B.C.E.");
   //Code in here executes when the user clicks the "Demosthenes" button.
 });
+
+var greatSpeeches = [
+  {
+    "author" : "Churchill",
+    "year"   : "1940",
+    "bce"    :  false,
+    "title"  : "Their Finest Hour"
+  },
+  {
+    "author" : "Ghandi",
+    "year"   : "1942",
+    "bce"    :  false,
+    "title"  : "Quit India"
+  },
+  {
+    "author" : "Demosthenes",
+    "year"   : "342",
+    "bce"    :  true,
+    "title"  : "The Third Philippic"
+  }
+];
+
+var differenceInYears = greatSpeeches[1].year-greatSpeeches[0].year;
+
+window.onload = function() {
+  console.log("Ghandi's speech and Churchill's speech are " + differenceInYears + " years apart.")
+};
